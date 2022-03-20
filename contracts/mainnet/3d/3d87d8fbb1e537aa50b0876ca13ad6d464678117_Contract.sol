c3ed4edacd17181b2def919eb8970367dad7f238
minion <minion>
2022-03-20 21:16:55 +0000

    with 💕

---
+++ contracts/mainnet/3d/3d87d8fbb1e537aa50b0876ca13ad6d464678117_Contract.sol
@@ -0,0 +1,13 @@
+/**
+ *Submitted for verification at optimistic.etherscan.io on 2022-03-20
+*/
+
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.12;
+
+contract Contract {
+
+    function helloWorld() external pure returns (string memory) {
+        return "Hello, world!";
+    }
+}
