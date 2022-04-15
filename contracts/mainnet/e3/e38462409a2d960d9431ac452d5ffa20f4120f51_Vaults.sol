bd2eda22c78511883d50e7da3e4c782bf64a63c6
minion <minion>
2022-04-15 21:10:18 +0000

    with 💕

---
+++ contracts/mainnet/e3/e38462409a2d960d9431ac452d5ffa20f4120f51_Vaults.sol
@@ -0,0 +1,521 @@
+/**
+ *Submitted for verification at optimistic.etherscan.io on 2022-04-15
+*/
+
+/**
+ *Submitted for verification at optimistic.etherscan.io on 2022-04-07
+*/
+
+/**
+ *Submitted for verification at kovan-optimistic.etherscan.io on 2022-03-10
+*/
+
+// Sources flattened with hardhat v2.8.3 https://hardhat.org
+
+// File @rari-capital/solmate/src/utils/[email protected]
+
+pragma solidity >=0.8.0;
+
+/// @notice Arithmetic library with operations for fixed-point numbers.
+/// @author Solmate (https://github.com/Rari-Capital/solmate/blob/main/src/utils/FixedPointMathLib.sol)
+library FixedPointMathLib {
+    /*///////////////////////////////////////////////////////////////
+                            COMMON BASE UNITS
+    //////////////////////////////////////////////////////////////*/
+
+    uint256 internal constant YAD = 1e8;
+    uint256 internal constant WAD = 1e18;
+    uint256 internal constant RAY = 1e27;
+    uint256 internal constant RAD = 1e45;
+
+    /*///////////////////////////////////////////////////////////////
+                         FIXED POINT OPERATIONS
+    //////////////////////////////////////////////////////////////*/
+
+    function fmul(
+        uint256 x,
+        uint256 y,
+        uint256 baseUnit
+    ) internal pure returns (uint256 z) {
+        assembly {
+            // Store x * y in z for now.
+            z := mul(x, y)
+
+            // Equivalent to require(x == 0 || (x * y) / x == y)
+            if iszero(or(iszero(x), eq(div(z, x), y))) {
+                revert(0, 0)
+            }
+
+            // If baseUnit is zero this will return zero instead of reverting.
+            z := div(z, baseUnit)
+        }
+    }
+
+    function fdiv(
+        uint256 x,
+        uint256 y,
+        uint256 baseUnit
+    ) internal pure returns (uint256 z) {
+        assembly {
+            // Store x * baseUnit in z for now.
+            z := mul(x, baseUnit)
+
+            // Equivalent to require(y != 0 && (x == 0 || (x * baseUnit) / x == baseUnit))
+            if iszero(and(iszero(iszero(y)), or(iszero(x), eq(div(z, x), baseUnit)))) {
+                revert(0, 0)
+            }
+
+            // We ensure y is not zero above, so there is never division by zero here.
+            z := div(z, y)
+        }
+    }
+
+    function fpow(
+        uint256 x,
+        uint256 n,
+        uint256 baseUnit
+    ) internal pure returns (uint256 z) {
+        assembly {
+            switch x
+            case 0 {
+                switch n
+                case 0 {
+                    // 0 ** 0 = 1
+                    z := baseUnit
+                }
+                default {
+                    // 0 ** n = 0
+                    z := 0
+                }
+            }
+            default {
+                switch mod(n, 2)
+                case 0 {
+                    // If n is even, store baseUnit in z for now.
+                    z := baseUnit
+                }
+                default {
+                    // If n is odd, store x in z for now.
+                    z := x
+                }
+
+                // Shifting right by 1 is like dividing by 2.
+                let half := shr(1, baseUnit)
+
+                for {
+                    // Shift n right by 1 before looping to halve it.
+                    n := shr(1, n)
+                } n {
+                    // Shift n right by 1 each iteration to halve it.
+                    n := shr(1, n)
+                } {
+                    // Revert immediately if x ** 2 would overflow.
+                    // Equivalent to iszero(eq(div(xx, x), x)) here.
+                    if shr(128, x) {
+                        revert(0, 0)
+                    }
+
+                    // Store x squared.
+                    let xx := mul(x, x)
+
+                    // Round to the nearest number.
+                    let xxRound := add(xx, half)
+
+                    // Revert if xx + half overflowed.
+                    if lt(xxRound, xx) {
+                        revert(0, 0)
+                    }
+
+                    // Set x to scaled xxRound.
+                    x := div(xxRound, baseUnit)
+
+                    // If n is even:
+                    if mod(n, 2) {
+                        // Compute z * x.
+                        let zx := mul(z, x)
+
+                        // If z * x overflowed:
+                        if iszero(eq(div(zx, x), z)) {
+                            // Revert if x is non-zero.
+                            if iszero(iszero(x)) {
+                                revert(0, 0)
+                            }
+                        }
+
+                        // Round to the nearest number.
+                        let zxRound := add(zx, half)
+
+                        // Revert if zx + half overflowed.
+                        if lt(zxRound, zx) {
+                            revert(0, 0)
+                        }
+
+                        // Return properly scaled zxRound.
+                        z := div(zxRound, baseUnit)
+                    }
+                }
+            }
+        }
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                        GENERAL NUMBER UTILITIES
+    //////////////////////////////////////////////////////////////*/
+
+    function sqrt(uint256 x) internal pure returns (uint256 z) {
+        assembly {
+            // Start off with z at 1.
+            z := 1
+
+            // Used below to help find a nearby power of 2.
+            let y := x
+
+            // Find the lowest power of 2 that is at least sqrt(x).
+            if iszero(lt(y, 0x100000000000000000000000000000000)) {
+                y := shr(128, y) // Like dividing by 2 ** 128.
+                z := shl(64, z)
+            }
+            if iszero(lt(y, 0x10000000000000000)) {
+                y := shr(64, y) // Like dividing by 2 ** 64.
+                z := shl(32, z)
+            }
+            if iszero(lt(y, 0x100000000)) {
+                y := shr(32, y) // Like dividing by 2 ** 32.
+                z := shl(16, z)
+            }
+            if iszero(lt(y, 0x10000)) {
+                y := shr(16, y) // Like dividing by 2 ** 16.
+                z := shl(8, z)
+            }
+            if iszero(lt(y, 0x100)) {
+                y := shr(8, y) // Like dividing by 2 ** 8.
+                z := shl(4, z)
+            }
+            if iszero(lt(y, 0x10)) {
+                y := shr(4, y) // Like dividing by 2 ** 4.
+                z := shl(2, z)
+            }
+            if iszero(lt(y, 0x8)) {
+                // Equivalent to 2 ** z.
+                z := shl(1, z)
+            }
+
+            // Shifting right by 1 is like dividing by 2.
+            z := shr(1, add(z, div(x, z)))
+            z := shr(1, add(z, div(x, z)))
+            z := shr(1, add(z, div(x, z)))
+            z := shr(1, add(z, div(x, z)))
+            z := shr(1, add(z, div(x, z)))
+            z := shr(1, add(z, div(x, z)))
+            z := shr(1, add(z, div(x, z)))
+
+            // Compute a rounded down version of z.
+            let zRoundDown := div(x, z)
+
+            // If zRoundDown is smaller, use it.
+            if lt(zRoundDown, z) {
+                z := zRoundDown
+            }
+        }
+    }
+}
+
+
+// File @rari-capital/solmate/src/tokens/[email protected]
+
+pragma solidity >=0.8.0;
+
+/// @notice Modern and gas efficient ERC20 + EIP-2612 implementation.
+/// @author Solmate (https://github.com/Rari-Capital/solmate/blob/main/src/tokens/ERC20.sol)
+/// @author Modified from Uniswap (https://github.com/Uniswap/uniswap-v2-core/blob/master/contracts/UniswapV2ERC20.sol)
+/// @dev Do not manually set balances without updating totalSupply, as the sum of all user balances must not exceed it.
+abstract contract ERC20 {
+    /*///////////////////////////////////////////////////////////////
+                                  EVENTS
+    //////////////////////////////////////////////////////////////*/
+
+    event Transfer(address indexed from, address indexed to, uint256 amount);
+
+    event Approval(address indexed owner, address indexed spender, uint256 amount);
+
+    /*///////////////////////////////////////////////////////////////
+                             METADATA STORAGE
+    //////////////////////////////////////////////////////////////*/
+
+    string public name;
+
+    string public symbol;
+
+    uint8 public immutable decimals;
+
+    /*///////////////////////////////////////////////////////////////
+                              ERC20 STORAGE
+    //////////////////////////////////////////////////////////////*/
+
+    uint256 public totalSupply;
+
+    mapping(address => uint256) public balanceOf;
+
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    /*///////////////////////////////////////////////////////////////
+                             EIP-2612 STORAGE
+    //////////////////////////////////////////////////////////////*/
+
+    bytes32 public constant PERMIT_TYPEHASH =
+        keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
+
+    uint256 internal immutable INITIAL_CHAIN_ID;
+
+    bytes32 internal immutable INITIAL_DOMAIN_SEPARATOR;
+
+    mapping(address => uint256) public nonces;
+
+    /*///////////////////////////////////////////////////////////////
+                               CONSTRUCTOR
+    //////////////////////////////////////////////////////////////*/
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint8 _decimals
+    ) {
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+
+        INITIAL_CHAIN_ID = block.chainid;
+        INITIAL_DOMAIN_SEPARATOR = computeDomainSeparator();
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                              ERC20 LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+    function approve(address spender, uint256 amount) public virtual returns (bool) {
+        allowance[msg.sender][spender] = amount;
+
+        emit Approval(msg.sender, spender, amount);
+
+        return true;
+    }
+
+    function transfer(address to, uint256 amount) public virtual returns (bool) {
+        balanceOf[msg.sender] -= amount;
+
+        // Cannot overflow because the sum of all user
+        // balances can't exceed the max uint256 value.
+        unchecked {
+            balanceOf[to] += amount;
+        }
+
+        emit Transfer(msg.sender, to, amount);
+
+        return true;
+    }
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) public virtual returns (bool) {
+        uint256 allowed = allowance[from][msg.sender]; // Saves gas for limited approvals.
+
+        if (allowed != type(uint256).max) allowance[from][msg.sender] = allowed - amount;
+
+        balanceOf[from] -= amount;
+
+        // Cannot overflow because the sum of all user
+        // balances can't exceed the max uint256 value.
+        unchecked {
+            balanceOf[to] += amount;
+        }
+
+        emit Transfer(from, to, amount);
+
+        return true;
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                              EIP-2612 LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+    function permit(
+        address owner,
+        address spender,
+        uint256 value,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) public virtual {
+        require(deadline >= block.timestamp, "PERMIT_DEADLINE_EXPIRED");
+
+        // Unchecked because the only math done is incrementing
+        // the owner's nonce which cannot realistically overflow.
+        unchecked {
+            bytes32 digest = keccak256(
+                abi.encodePacked(
+                    "\x19\x01",
+                    DOMAIN_SEPARATOR(),
+                    keccak256(abi.encode(PERMIT_TYPEHASH, owner, spender, value, nonces[owner]++, deadline))
+                )
+            );
+
+            address recoveredAddress = ecrecover(digest, v, r, s);
+
+            require(recoveredAddress != address(0) && recoveredAddress == owner, "INVALID_SIGNER");
+
+            allowance[recoveredAddress][spender] = value;
+        }
+
+        emit Approval(owner, spender, value);
+    }
+
+    function DOMAIN_SEPARATOR() public view virtual returns (bytes32) {
+        return block.chainid == INITIAL_CHAIN_ID ? INITIAL_DOMAIN_SEPARATOR : computeDomainSeparator();
+    }
+
+    function computeDomainSeparator() internal view virtual returns (bytes32) {
+        return
+            keccak256(
+                abi.encode(
+                    keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
+                    keccak256(bytes(name)),
+                    keccak256("1"),
+                    block.chainid,
+                    address(this)
+                )
+            );
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                       INTERNAL MINT/BURN LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+    function _mint(address to, uint256 amount) internal virtual {
+        totalSupply += amount;
+
+        // Cannot overflow because the sum of all user
+        // balances can't exceed the max uint256 value.
+        unchecked {
+            balanceOf[to] += amount;
+        }
+
+        emit Transfer(address(0), to, amount);
+    }
+
+    function _burn(address from, uint256 amount) internal virtual {
+        balanceOf[from] -= amount;
+
+        // Cannot underflow because a user's balance
+        // will never be larger than the total supply.
+        unchecked {
+            totalSupply -= amount;
+        }
+
+        emit Transfer(from, address(0), amount);
+    }
+}
+
+
+// File contracts/resolvers/Vaults.sol
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.9;
+
+
+interface IVault {
+    function currentRound() external view returns (uint256);
+
+    function userInfos(address _user) external view returns (
+        uint256 depositRound,
+        uint256 pendingDeposit,
+        uint256 withdrawRound,
+        uint256 withdrawnShares,
+        uint256 totalShares
+    );
+
+    function performanceIndices(uint256 _round) external view returns (uint256);
+}
+
+contract Vaults {
+    using FixedPointMathLib for uint256;
+
+    function getAllBalances(
+        address _user,
+        IVault[] memory _vaults,
+        ERC20[] memory _tokens
+    ) public view returns (
+        uint256[] memory _vaultBalances,
+        uint256[] memory _vaultShares,
+        uint256[] memory _vaultWithdrawsToComplete,
+        uint256[] memory _vaultCancellableWithdraw,
+        uint256[] memory _balances
+    ) {
+        _balances = getTokenBalances(_user, _tokens);
+        (_vaultBalances, _vaultShares, _vaultWithdrawsToComplete, _vaultCancellableWithdraw) = getUserBalances(_user, _vaults);
+    }
+
+    function getUserBalance(address _user, IVault _vault) public view returns (
+        uint256 _balance,
+        uint256 _shares,
+        uint256 _withdrawToComplete,
+        uint256 _cancellableWithdraw
+    ) {
+        (uint256 _depositRound, uint256 _pendingDeposit, uint256 _withdrawRound, uint256 _withdrawnShares, uint256 _totalShares) = _vault.userInfos(_user);
+        uint256 _currentRound = _vault.currentRound();
+        _shares = _totalShares;
+
+        if (_pendingDeposit > 0 && _depositRound < _currentRound) {
+            uint256 _index = _vault.performanceIndices(_depositRound);
+            _shares += _pendingDeposit.fdiv(_index, 1e18);
+            _pendingDeposit = 0;
+        }
+
+        uint256 _currentIndex = _currentRound > 0 ? _vault.performanceIndices(_currentRound - 1) : 1e18;
+        _balance = _shares.fmul(_currentIndex, 1e18) + _pendingDeposit;
+        _withdrawToComplete = _currentRound > _withdrawRound ? _withdrawnShares.fmul(_currentIndex, 1e18) : 0;
+        _cancellableWithdraw = _currentRound == _withdrawRound ? _withdrawnShares.fmul(_currentIndex, 1e18) : 0;
+    }
+
+    function getUserBalances(address _user, IVault[] memory _vaults) public view returns (
+        uint256[] memory _balances,
+        uint256[] memory _shares,
+        uint256[] memory _withdrawToComplete,
+        uint256[] memory _cancellableWithdraw
+    ) {
+        _balances = new uint256[](_vaults.length);
+        _shares = new uint256[](_vaults.length);
+        _withdrawToComplete = new uint256[](_vaults.length);
+        _cancellableWithdraw = new uint256[](_vaults.length);
+
+        for (uint256 i = 0; i < _vaults.length; i++) {
+            (_balances[i], _shares[i], _withdrawToComplete[i], _cancellableWithdraw[i]) = getUserBalance(_user, _vaults[i]);
+        }
+    }
+
+    function getTokenBalances(address _user, ERC20[] memory _tokens) public view returns (uint256[] memory _balances) {
+        _balances = new uint256[](_tokens.length);
+
+        for (uint256 i = 0; i < _tokens.length; i++) {
+            _balances[i] = _tokens[i].balanceOf(_user);
+        }
+    }
+
+    function getPerformance(uint256 _rounds, IVault _vault) public view returns (uint256[] memory _indices) {
+        uint256 _currentRound = _vault.currentRound();
+        if (_currentRound == 0 || _currentRound == 1) {
+            _indices = new uint256[](1);
+            _indices[0] = 1e18;
+        } else {
+            _rounds = _rounds > _currentRound ? _currentRound : _rounds;
+
+            _indices = new uint256[](_rounds);
+
+            for (uint256 i = 0; i < _rounds; i++) {
+                _indices[i] = _vault.performanceIndices(i);
+            }
+        }
+    }
+}
