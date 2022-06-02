d0179a9554e1a428582a28fbe18830c9263eeb25
minion <minion>
2022-06-02 21:08:08 +0000

    with 💕

---
+++ contracts/mainnet/00/00D59BC35174C3b250Dd92a363495d38C8777a49_MerkleClaim.sol
@@ -0,0 +1,216 @@
+/**
+ *Submitted for verification at optimistic.etherscan.io on 2022-06-01
+*/
+
+// File: openzeppelin-contracts/contracts/utils/cryptography/MerkleProof.sol
+
+
+// OpenZeppelin Contracts (last updated v4.6.0) (utils/cryptography/MerkleProof.sol)
+
+pragma solidity ^0.8.0;
+
+/**
+ * @dev These functions deal with verification of Merkle Trees proofs.
+ *
+ * The proofs can be generated using the JavaScript library
+ * https://github.com/miguelmota/merkletreejs[merkletreejs].
+ * Note: the hashing algorithm should be keccak256 and pair sorting should be enabled.
+ *
+ * See `test/utils/cryptography/MerkleProof.test.js` for some examples.
+ *
+ * WARNING: You should avoid using leaf values that are 64 bytes long prior to
+ * hashing, or use a hash function other than keccak256 for hashing leaves.
+ * This is because the concatenation of a sorted pair of internal nodes in
+ * the merkle tree could be reinterpreted as a leaf value.
+ */
+library MerkleProof {
+    /**
+     * @dev Returns true if a `leaf` can be proved to be a part of a Merkle tree
+     * defined by `root`. For this, a `proof` must be provided, containing
+     * sibling hashes on the branch from the leaf to the root of the tree. Each
+     * pair of leaves and each pair of pre-images are assumed to be sorted.
+     */
+    function verify(
+        bytes32[] memory proof,
+        bytes32 root,
+        bytes32 leaf
+    ) internal pure returns (bool) {
+        return processProof(proof, leaf) == root;
+    }
+
+    /**
+     * @dev Returns the rebuilt hash obtained by traversing a Merkle tree up
+     * from `leaf` using `proof`. A `proof` is valid if and only if the rebuilt
+     * hash matches the root of the tree. When processing the proof, the pairs
+     * of leafs & pre-images are assumed to be sorted.
+     *
+     * _Available since v4.4._
+     */
+    function processProof(bytes32[] memory proof, bytes32 leaf) internal pure returns (bytes32) {
+        bytes32 computedHash = leaf;
+        for (uint256 i = 0; i < proof.length; i++) {
+            computedHash = _hashPair(computedHash, proof[i]);
+        }
+        return computedHash;
+    }
+
+    /**
+     * @dev Returns true if a `leafs` can be proved to be a part of a Merkle tree
+     * defined by `root`. For this, `proofs` for each leaf must be provided, containing
+     * sibling hashes on the branch from the leaf to the root of the tree. Then
+     * 'proofFlag' designates the nodes needed for the multi proof.
+     *
+     * _Available since v4.7._
+     */
+    function multiProofVerify(
+        bytes32 root,
+        bytes32[] memory leafs,
+        bytes32[] memory proofs,
+        bool[] memory proofFlag
+    ) internal pure returns (bool) {
+        return processMultiProof(leafs, proofs, proofFlag) == root;
+    }
+
+    /**
+     * @dev Returns the rebuilt hash obtained by traversing a Merkle tree up
+     * from `leaf` using the multi proof as `proofFlag`. A multi proof is
+     * valid if the final hash matches the root of the tree.
+     *
+     * _Available since v4.7._
+     */
+    function processMultiProof(
+        bytes32[] memory leafs,
+        bytes32[] memory proofs,
+        bool[] memory proofFlag
+    ) internal pure returns (bytes32 merkleRoot) {
+        // This function rebuild the root hash by traversing the tree up from the leaves. The root is rebuilt by
+        // consuming and producing values on a queue. The queue starts with the `leafs` array, then goes onto the
+        // `hashes` array. At the end of the process, the last hash in the `hashes` array should contain the root of
+        // the merkle tree.
+        uint256 leafsLen = leafs.length;
+        uint256 proofsLen = proofs.length;
+        uint256 totalHashes = proofFlag.length;
+
+        // Check proof validity.
+        require(leafsLen + proofsLen - 1 == totalHashes, "MerkleProof: invalid multiproof");
+
+        // The xxxPos values are "pointers" to the next value to consume in each array. All accesses are done using
+        // `xxx[xxxPos++]`, which return the current value and increment the pointer, thus mimicking a queue's "pop".
+        bytes32[] memory hashes = new bytes32[](totalHashes);
+        uint256 leafPos = 0;
+        uint256 hashPos = 0;
+        uint256 proofPos = 0;
+        // At each step, we compute the next hash using two values:
+        // - a value from the "main queue". If not all leaves have been consumed, we get the next leaf, otherwise we
+        //   get the next hash.
+        // - depending on the flag, either another value for the "main queue" (merging branches) or an element from the
+        //   `proofs` array.
+        for (uint256 i = 0; i < totalHashes; i++) {
+            bytes32 a = leafPos < leafsLen ? leafs[leafPos++] : hashes[hashPos++];
+            bytes32 b = proofFlag[i] ? leafPos < leafsLen ? leafs[leafPos++] : hashes[hashPos++] : proofs[proofPos++];
+            hashes[i] = _hashPair(a, b);
+        }
+
+        return hashes[totalHashes - 1];
+    }
+
+    function _hashPair(bytes32 a, bytes32 b) private pure returns (bytes32) {
+        return a < b ? _efficientHash(a, b) : _efficientHash(b, a);
+    }
+
+    function _efficientHash(bytes32 a, bytes32 b) private pure returns (bytes32 value) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            mstore(0x00, a)
+            mstore(0x20, b)
+            value := keccak256(0x00, 0x40)
+        }
+    }
+}
+
+// File: contracts/interfaces/IVelo.sol
+
+pragma solidity 0.8.13;
+
+interface IVelo {
+    function totalSupply() external view returns (uint);
+    function balanceOf(address) external view returns (uint);
+    function approve(address spender, uint value) external returns (bool);
+    function transfer(address, uint) external returns (bool);
+    function transferFrom(address,address,uint) external returns (bool);
+    function mint(address, uint) external returns (bool);
+    function minter() external returns (address);
+    function claim(address, uint) external returns (bool);
+}
+
+// File: contracts/redeem/MerkleClaim.sol
+
+
+pragma solidity 0.8.13;
+
+/// ============ Imports ============
+
+
+
+/// @title MerkleClaim
+/// @notice Claims VELO for members of a merkle tree
+/// @author Modified from Merkle Airdrop Starter (https://github.com/Anish-Agnihotri/merkle-airdrop-starter/blob/master/contracts/src/MerkleClaimERC20.sol)
+contract MerkleClaim {
+    /// ============ Immutable storage ============
+
+    /// @notice VELO token to claim
+    IVelo public immutable VELO;
+    /// @notice ERC20-claimee inclusion root
+    bytes32 public immutable merkleRoot;
+
+    /// ============ Mutable storage ============
+
+    /// @notice Mapping of addresses who have claimed tokens
+    mapping(address => bool) public hasClaimed;
+
+    /// ============ Constructor ============
+
+    /// @notice Creates a new MerkleClaim contract
+    /// @param _velo address
+    /// @param _merkleRoot of claimees
+    constructor(address _velo, bytes32 _merkleRoot) {
+        VELO = IVelo(_velo);
+        merkleRoot = _merkleRoot;
+    }
+
+    /// ============ Events ============
+
+    /// @notice Emitted after a successful token claim
+    /// @param to recipient of claim
+    /// @param amount of tokens claimed
+    event Claim(address indexed to, uint256 amount);
+
+    /// ============ Functions ============
+
+    /// @notice Allows claiming tokens if address is part of merkle tree
+    /// @param to address of claimee
+    /// @param amount of tokens owed to claimee
+    /// @param proof merkle proof to prove address and amount are in tree
+    function claim(
+        address to,
+        uint256 amount,
+        bytes32[] calldata proof
+    ) external {
+        // Throw if address has already claimed tokens
+        require(!hasClaimed[to], "ALREADY_CLAIMED");
+
+        // Verify merkle proof, or revert if not in tree
+        bytes32 leaf = keccak256(abi.encodePacked(to, amount));
+        bool isValidLeaf = MerkleProof.verify(proof, merkleRoot, leaf);
+        require(isValidLeaf, "NOT_IN_MERKLE");
+
+        // Set address to claimed
+        hasClaimed[to] = true;
+
+        // Claim tokens for address
+        require(VELO.claim(to, amount), "CLAIM_FAILED");
+
+        // Emit claim event
+        emit Claim(to, amount);
+    }
+}
