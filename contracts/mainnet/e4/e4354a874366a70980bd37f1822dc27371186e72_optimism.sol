7467aba04495fd39b33236b9d64c0052417597f7
minion <minion>
2022-07-31 11:05:10 +0000

    with 💕

---
+++ contracts/mainnet/e4/e4354a874366a70980bd37f1822dc27371186e72_optimism.sol
@@ -0,0 +1,11 @@
+/**
+ *Submitted for verification at Optimistic.Etherscan.io on 2022-07-31
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.8;
+contract optimism {
+    function get()public pure returns (string memory){
+        return 'gM optimism';
+    }
+}
