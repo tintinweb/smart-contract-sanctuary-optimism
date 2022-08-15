f97432c4eb217060310aeec32b9dd1d7cbb8a406
minion <minion>
2022-08-15 17:15:47 +0000

    with 💕

---
+++ contracts/mainnet/63/63500d833140bdd69e6fbf3039b111c703333996_OPc.sol
@@ -0,0 +1,11 @@
+/**
+ *Submitted for verification at Optimistic.Etherscan.io on 2022-08-15
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.8;
+contract OPc {
+    function get()public pure returns (string memory){
+        return 'gM OPc';
+    }
+}
