757a213a73996533b8b04c0661c2379130353c48
minion <minion>
2022-09-19 11:08:39 +0000

    with 💕

---
+++ contracts/mainnet/61/6163f43923aa5d0281fc1e3afa00d785558879e9_HelloOP.sol
@@ -0,0 +1,12 @@
+/**
+ *Submitted for verification at Optimistic.Etherscan.io on 2022-09-19
+*/
+
+// TestingTheZksync 
+pragma solidity >=0.5.0 <0.7.0;
+contract HelloOP {
+    function gm()public pure returns (string memory){
+        return 'Hello it is my test smart contract in Optimism network';
+    }
+    
+}
