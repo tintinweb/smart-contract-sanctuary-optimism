1f6cf156db8f7be66174b5f28e9a5bb64cd33f20
minion <minion>
2022-09-26 05:33:48 +0000

    with 💕

---
+++ contracts/mainnet/5b/5b59A43b164089Efd6C62CD1716D30Ec25c55d1A_VelodromeStableLPAggregator.sol
@@ -0,0 +1,563 @@
+// SPDX-License-Identifier: BUSL-1.1
+
+pragma solidity 0.7.6;
+
+import "@openzeppelin/contracts-upgradeable/math/SafeMathUpgradeable.sol";
+
+import "../interfaces/IAggregatorV3Interface.sol";
+import "../interfaces/velodrome/IVelodromePair.sol";
+import "../interfaces/IERC20Extended.sol"; // includes decimals()
+import "../interfaces/IHasAssetInfo.sol";
+import "../utils/DhedgeMath.sol";
+
+/**
+ * @title Velodrome Stable LP aggregator. For dHEDGE LP Price Feeds.
+ * @notice You can use this contract for Velodrome lp token pricing oracle.
+ * @dev This should have `latestRoundData` function as chainlink pricing oracle.
+ */
+contract VelodromeStableLPAggregator is IAggregatorV3Interface {
+  using SafeMathUpgradeable for uint256;
+
+  address public pair;
+  address public token0;
+  address public token1;
+  address public factory;
+
+  constructor(address _pair, address _factory) {
+    require(_pair != address(0), "_pair address cannot be 0");
+    require(_factory != address(0), "_factory address cannot be 0");
+    pair = _pair;
+    token0 = IVelodromePair(pair).token0();
+    token1 = IVelodromePair(pair).token1();
+    factory = _factory;
+  }
+
+  /* ========== VIEWS ========== */
+
+  function decimals() external pure override returns (uint8) {
+    return 8;
+  }
+
+  function _calculateFairReserves(
+    uint256 x,
+    uint256 y,
+    uint256 px,
+    uint256 py
+  ) private pure returns (uint256 fairX, uint256 fairY) {
+    // NOTE:
+    // x = reserve0 (18 decimals), y = reserve1 (18 decimals), px = token0 price (18 decimals), py = token1 price (18 decimals)
+    // constant product = x^3 * y + x * y^3
+    // constraints:
+    // - fairX^3 * fairY + fairX * fairY^3 = constant product
+    // - fairX * px = fairY * py
+    // Solving equations:
+    // --> fairY = fairX * px / py
+    // --> fairX^4 * px / py + fairX^4 * (px / py)^3 = constant product
+    // --> ratio = px / py
+    // --> fairX^4 * (ratio + ratio^3) = x * y * (x^2 + y^2)
+    // --> fairX^2 = sqrt(x * y) * sqrt(x^2 + y^2) / (sqrt(ratio) * sqrt(1 + ratio^2))
+    // --> fairX = sqrt(sqrt(x * y) * sqrt(x^2 + y^2)) / sqrt(sqrt(ratio) * sqrt(1 + ratio^2))
+
+    // r0 = sqrt(x * y)
+    uint256 r0 = DhedgeMath.sqrt(x.mul(y)); // decimal = 18
+    // r1 = sqrt(x^2 + y^2)
+    uint256 r1 = DhedgeMath.sqrt(x.mul(x) + y.mul(y)); // decimal = 18
+    // r = sqrt(sqrt(x * y) * sqrt(x^2 + y^2))
+    uint256 r = DhedgeMath.sqrt(r0.mul(r1)); // decimal = 18
+
+    // ratio = px / py
+    uint256 ratio = px.mul(10**18).div(py); // decimal = 18
+    // p0 = sqrt(ratio)
+    uint256 p0 = DhedgeMath.sqrt(ratio.mul(10**18)); // decimal = 18
+    // p1 = sqrt(1 + ratio^2)
+    uint256 p1 = DhedgeMath.sqrt(10**36 + ratio.mul(ratio)); // decimal = 18
+    // p = sqrt(sqrt(ratio) * sqrt(1 + ratio^2))
+    uint256 p = DhedgeMath.sqrt(p0.mul(p1)); // decimal = 18
+
+    // fairX = sqrt(sqrt(x * y) * sqrt(x^2 + y^2)) / sqrt(sqrt(ratio) * sqrt(1 + ratio^2))
+    fairX = r.mul(10**18).div(p);
+    fairY = fairX.mul(px).div(py);
+  }
+
+  /**
+   * @notice Get the latest round data. Should be the same format as chainlink aggregator.
+   * @return roundId The round ID.
+   * @return answer The price - the latest round data of a given velodrome lp token (price decimal: 8)
+   * @return startedAt Timestamp of when the round started.
+   * @return updatedAt Timestamp of when the round was updated.
+   * @return answeredInRound The round ID of the round in which the answer was computed.
+   */
+  function latestRoundData()
+    external
+    view
+    override
+    returns (
+      uint80,
+      int256,
+      uint256,
+      uint256,
+      uint80
+    )
+  {
+    (uint256 answer0, uint256 answer1) = _getTokenPrices();
+    uint256 totalSupply = IVelodromePair(pair).totalSupply();
+    (uint256 r0, uint256 r1, ) = IVelodromePair(pair).getReserves();
+    uint256 decimal0 = IERC20Extended(token0).decimals();
+    uint256 decimal1 = IERC20Extended(token1).decimals();
+
+    r0 = r0.mul(10**18).div(10**decimal0); // decimal = 18
+    r1 = r1.mul(10**18).div(10**decimal1); // decimal = 18
+
+    (uint256 fairX, uint256 fairY) = _calculateFairReserves(r0, r1, answer0, answer1);
+
+    uint256 answer = fairX.mul(answer0).add(fairY.mul(answer1)).div(totalSupply); // decimal = 18
+
+    // we don't need roundId, startedAt and answeredInRound
+    return (0, int256(answer.div(10**10)), 0, block.timestamp, 0);
+  }
+
+  /* ========== INTERNAL ========== */
+
+  function _getTokenPrices() internal view returns (uint256, uint256) {
+    return (IHasAssetInfo(factory).getAssetPrice(token0), IHasAssetInfo(factory).getAssetPrice(token1));
+  }
+}
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.7.0;
+
+/**
+ * @dev Wrappers over Solidity's arithmetic operations with added overflow
+ * checks.
+ *
+ * Arithmetic operations in Solidity wrap on overflow. This can easily result
+ * in bugs, because programmers usually assume that an overflow raises an
+ * error, which is the standard behavior in high level programming languages.
+ * `SafeMath` restores this intuition by reverting the transaction when an
+ * operation overflows.
+ *
+ * Using this library instead of the unchecked operations eliminates an entire
+ * class of bugs, so it's recommended to use it always.
+ */
+library SafeMathUpgradeable {
+    /**
+     * @dev Returns the addition of two unsigned integers, with an overflow flag.
+     *
+     * _Available since v3.4._
+     */
+    function tryAdd(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        uint256 c = a + b;
+        if (c < a) return (false, 0);
+        return (true, c);
+    }
+
+    /**
+     * @dev Returns the substraction of two unsigned integers, with an overflow flag.
+     *
+     * _Available since v3.4._
+     */
+    function trySub(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        if (b > a) return (false, 0);
+        return (true, a - b);
+    }
+
+    /**
+     * @dev Returns the multiplication of two unsigned integers, with an overflow flag.
+     *
+     * _Available since v3.4._
+     */
+    function tryMul(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+        // benefit is lost if 'b' is also tested.
+        // See: https://github.com/OpenZeppelin/openzeppelin-contracts/pull/522
+        if (a == 0) return (true, 0);
+        uint256 c = a * b;
+        if (c / a != b) return (false, 0);
+        return (true, c);
+    }
+
+    /**
+     * @dev Returns the division of two unsigned integers, with a division by zero flag.
+     *
+     * _Available since v3.4._
+     */
+    function tryDiv(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        if (b == 0) return (false, 0);
+        return (true, a / b);
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers, with a division by zero flag.
+     *
+     * _Available since v3.4._
+     */
+    function tryMod(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        if (b == 0) return (false, 0);
+        return (true, a % b);
+    }
+
+    /**
+     * @dev Returns the addition of two unsigned integers, reverting on
+     * overflow.
+     *
+     * Counterpart to Solidity's `+` operator.
+     *
+     * Requirements:
+     *
+     * - Addition cannot overflow.
+     */
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a, "SafeMath: addition overflow");
+        return c;
+    }
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, reverting on
+     * overflow (when the result is negative).
+     *
+     * Counterpart to Solidity's `-` operator.
+     *
+     * Requirements:
+     *
+     * - Subtraction cannot overflow.
+     */
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b <= a, "SafeMath: subtraction overflow");
+        return a - b;
+    }
+
+    /**
+     * @dev Returns the multiplication of two unsigned integers, reverting on
+     * overflow.
+     *
+     * Counterpart to Solidity's `*` operator.
+     *
+     * Requirements:
+     *
+     * - Multiplication cannot overflow.
+     */
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) return 0;
+        uint256 c = a * b;
+        require(c / a == b, "SafeMath: multiplication overflow");
+        return c;
+    }
+
+    /**
+     * @dev Returns the integer division of two unsigned integers, reverting on
+     * division by zero. The result is rounded towards zero.
+     *
+     * Counterpart to Solidity's `/` operator. Note: this function uses a
+     * `revert` opcode (which leaves remaining gas untouched) while Solidity
+     * uses an invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b > 0, "SafeMath: division by zero");
+        return a / b;
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+     * reverting when dividing by zero.
+     *
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+     * invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b > 0, "SafeMath: modulo by zero");
+        return a % b;
+    }
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, reverting with custom message on
+     * overflow (when the result is negative).
+     *
+     * CAUTION: This function is deprecated because it requires allocating memory for the error
+     * message unnecessarily. For custom revert reasons use {trySub}.
+     *
+     * Counterpart to Solidity's `-` operator.
+     *
+     * Requirements:
+     *
+     * - Subtraction cannot overflow.
+     */
+    function sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b <= a, errorMessage);
+        return a - b;
+    }
+
+    /**
+     * @dev Returns the integer division of two unsigned integers, reverting with custom message on
+     * division by zero. The result is rounded towards zero.
+     *
+     * CAUTION: This function is deprecated because it requires allocating memory for the error
+     * message unnecessarily. For custom revert reasons use {tryDiv}.
+     *
+     * Counterpart to Solidity's `/` operator. Note: this function uses a
+     * `revert` opcode (which leaves remaining gas untouched) while Solidity
+     * uses an invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b > 0, errorMessage);
+        return a / b;
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+     * reverting with custom message when dividing by zero.
+     *
+     * CAUTION: This function is deprecated because it requires allocating memory for the error
+     * message unnecessarily. For custom revert reasons use {tryMod}.
+     *
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+     * invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function mod(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b > 0, errorMessage);
+        return a % b;
+    }
+}
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.7.6;
+
+interface IAggregatorV3Interface {
+  function decimals() external view returns (uint8);
+
+  function latestRoundData()
+    external
+    view
+    returns (
+      uint80 roundId,
+      int256 answer,
+      uint256 startedAt,
+      uint256 updatedAt,
+      uint80 answeredInRound
+    );
+}
+
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity 0.7.6;
+
+interface IVelodromePair {
+  function token0() external view returns (address);
+
+  function token1() external view returns (address);
+
+  function claimable0(address user) external view returns (uint256);
+
+  function claimable1(address user) external view returns (uint256);
+
+  function totalSupply() external view returns (uint256);
+
+  function current(address tokenIn, uint256 amountIn) external view returns (uint256 amountOut);
+
+  function burn(address to) external returns (uint256 amount0, uint256 amount1);
+
+  function getReserves()
+    external
+    view
+    returns (
+      uint112 reserve0,
+      uint112 reserve1,
+      uint32 blockTimestampLast
+    );
+
+  function claimFees() external returns (uint256 claimed0, uint256 claimed1);
+}
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.7.6;
+
+// With aditional optional views
+
+interface IERC20Extended {
+  // ERC20 Optional Views
+  function name() external view returns (string memory);
+
+  function symbol() external view returns (string memory);
+
+  function decimals() external view returns (uint8);
+
+  // Views
+  function totalSupply() external view returns (uint256);
+
+  function balanceOf(address owner) external view returns (uint256);
+
+  function scaledBalanceOf(address user) external view returns (uint256);
+
+  function allowance(address owner, address spender) external view returns (uint256);
+
+  // Mutative functions
+  function transfer(address to, uint256 value) external returns (bool);
+
+  function approve(address spender, uint256 value) external returns (bool);
+
+  function transferFrom(
+    address from,
+    address to,
+    uint256 value
+  ) external returns (bool);
+
+  // Events
+  event Transfer(address indexed from, address indexed to, uint256 value);
+
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+//
+//        __  __    __  ________  _______    ______   ________
+//       /  |/  |  /  |/        |/       \  /      \ /        |
+//   ____$$ |$$ |  $$ |$$$$$$$$/ $$$$$$$  |/$$$$$$  |$$$$$$$$/
+//  /    $$ |$$ |__$$ |$$ |__    $$ |  $$ |$$ | _$$/ $$ |__
+// /$$$$$$$ |$$    $$ |$$    |   $$ |  $$ |$$ |/    |$$    |
+// $$ |  $$ |$$$$$$$$ |$$$$$/    $$ |  $$ |$$ |$$$$ |$$$$$/
+// $$ \__$$ |$$ |  $$ |$$ |_____ $$ |__$$ |$$ \__$$ |$$ |_____
+// $$    $$ |$$ |  $$ |$$       |$$    $$/ $$    $$/ $$       |
+//  $$$$$$$/ $$/   $$/ $$$$$$$$/ $$$$$$$/   $$$$$$/  $$$$$$$$/
+//
+// dHEDGE DAO - https://dhedge.org
+//
+// Copyright (c) 2021 dHEDGE DAO
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.7.6;
+
+interface IHasAssetInfo {
+  function isValidAsset(address asset) external view returns (bool);
+
+  function getAssetPrice(address asset) external view returns (uint256);
+
+  function getAssetType(address asset) external view returns (uint16);
+
+  function getMaximumSupportedAssetCount() external view returns (uint256);
+}
+
+//        __  __    __  ________  _______    ______   ________
+//       /  |/  |  /  |/        |/       \  /      \ /        |
+//   ____$$ |$$ |  $$ |$$$$$$$$/ $$$$$$$  |/$$$$$$  |$$$$$$$$/
+//  /    $$ |$$ |__$$ |$$ |__    $$ |  $$ |$$ | _$$/ $$ |__
+// /$$$$$$$ |$$    $$ |$$    |   $$ |  $$ |$$ |/    |$$    |
+// $$ |  $$ |$$$$$$$$ |$$$$$/    $$ |  $$ |$$ |$$$$ |$$$$$/
+// $$ \__$$ |$$ |  $$ |$$ |_____ $$ |__$$ |$$ \__$$ |$$ |_____
+// $$    $$ |$$ |  $$ |$$       |$$    $$/ $$    $$/ $$       |
+//  $$$$$$$/ $$/   $$/ $$$$$$$$/ $$$$$$$/   $$$$$$/  $$$$$$$$/
+//
+// dHEDGE DAO - https://dhedge.org
+//
+// Copyright (c) 2021 dHEDGE DAO
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.7.6;
+
+/**
+ * @title A library for mathematical calculations.
+ * @dev For not only `sqrt` functionality is available. More functionalities can be added.
+ */
+library DhedgeMath {
+  /**
+   * @notice credit for this implementation goes to https://github.com/abdk-consulting/abdk-libraries-solidity/blob/master/ABDKMath64x64.sol
+   * @dev Calculate sqrt (x) rounding down, where x is unsigned 256-bit integer number.
+   * @param x unsigned 256-bit integer number
+   * @return sqrt(`x`) unsigned 128-bit integer number
+   */
+  function sqrt(uint256 x) internal pure returns (uint128) {
+    if (x == 0) return 0;
+    else {
+      uint256 xx = x;
+      uint256 r = 1;
+      if (xx >= 0x100000000000000000000000000000000) {
+        xx >>= 128;
+        r <<= 64;
+      }
+      if (xx >= 0x10000000000000000) {
+        xx >>= 64;
+        r <<= 32;
+      }
+      if (xx >= 0x100000000) {
+        xx >>= 32;
+        r <<= 16;
+      }
+      if (xx >= 0x10000) {
+        xx >>= 16;
+        r <<= 8;
+      }
+      if (xx >= 0x100) {
+        xx >>= 8;
+        r <<= 4;
+      }
+      if (xx >= 0x10) {
+        xx >>= 4;
+        r <<= 2;
+      }
+      if (xx >= 0x8) {
+        r <<= 1;
+      }
+      r = (r + x / r) >> 1;
+      r = (r + x / r) >> 1;
+      r = (r + x / r) >> 1;
+      r = (r + x / r) >> 1;
+      r = (r + x / r) >> 1;
+      r = (r + x / r) >> 1;
+      r = (r + x / r) >> 1; // Seven iterations should be enough
+      uint256 r1 = x / r;
+      return uint128(r < r1 ? r : r1);
+    }
+  }
+}
