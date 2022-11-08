1857e07123be3f42bad3df6eaaf69a497c8269ff
minion <minion>
2022-11-08 17:21:24 +0000

    with 💕

---
+++ contracts/mainnet/30/308B336DAA88848c2A809f517284b47c8f7c24e0_testMe.sol
@@ -0,0 +1,19 @@
+/**
+ *Submitted for verification at Optimistic.Etherscan.io on 2022-11-08
+*/
+
+pragma solidity ^0.8.4;
+
+error Unauthorized();
+
+contract testMe {
+    address payable owner = payable(msg.sender);
+    uint256 public increment;
+    
+    function add() public {
+        if (msg.sender != owner)
+            revert Unauthorized();
+
+        increment = increment + 1;
+    }
+}
