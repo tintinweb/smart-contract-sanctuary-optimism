ec15ea6613849f518ccdb253c051bf5e34a58ec6
minion <minion>
2022-11-17 17:14:08 +0000

    with 💕

---
+++ contracts/mainnet/57/57D8f155628AD921845Cb6D8cEB7f4A59878CA33_HelloWorld.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+// compiler version must be greater than or equal to 0.8.13 and less than 0.9.0
+pragma solidity 0.8.14;
+
+contract HelloWorld {
+    string public greet = "Hello World!";
+
+	function getValue() public view returns(uint){
+		return 42;
+	}
+}
