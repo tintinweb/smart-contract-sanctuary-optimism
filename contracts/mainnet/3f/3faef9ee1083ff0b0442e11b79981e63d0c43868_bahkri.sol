1c464931e799a5699900582510ca76b2553e1b8a
minion <minion>
2023-01-11 21:10:38 +0000

    with 💕

---
+++ contracts/mainnet/3f/3faef9ee1083ff0b0442e11b79981e63d0c43868_bahkri.sol
@@ -0,0 +1,28 @@
+/**
+ *Submitted for verification at Optimistic.Etherscan.io on 2023-01-11
+*/
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+
+contract bahkri {
+
+     address public owner;
+    mapping (address => uint) public payments;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function payforNFT() public payable {
+        payments[msg.sender] = msg.value;
+    }
+
+    function withdrawALL() public {
+        address payable _to = payable(owner);
+        address _thisContract = address(this);
+        _to.transfer(_thisContract.balance);
+    }
+}
