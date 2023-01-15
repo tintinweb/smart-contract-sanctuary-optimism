e9c98345b7e3767c3c0fa43dc26b79a3e49551c0
minion <minion>
2023-01-15 11:05:59 +0000

    with 💕

---
+++ contracts/mainnet/e3/e307c41e494ae1a7def4f03ed544a16e64667edb_myContractL2.sol
@@ -0,0 +1,37 @@
+/**
+ *Submitted for verification at Optimistic.Etherscan.io on 2023-01-15
+*/
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+contract myContractL2{
+    address public owner;
+    
+    
+    constructor(){
+        owner = msg.sender;
+    }
+    modifier onlyOwner{
+        require(owner == msg.sender, "not owner!");
+        _;
+    }
+    mapping (address => uint) payments;
+
+    function callMe()external pure returns(string memory){
+        return "Welcome to L2";
+    }
+    function onlyOwnerCan()public view onlyOwner returns(string memory){
+        return "Hello owner of L2 contract";
+    }
+
+    function inputFunds()public payable{
+        payments[msg.sender] = msg.value;
+    }
+     function withdrawAll() public onlyOwner{
+        address payable _to = payable(owner);
+        address _thisContract = address(this);
+        _to.transfer(_thisContract.balance);
+    }
+}
