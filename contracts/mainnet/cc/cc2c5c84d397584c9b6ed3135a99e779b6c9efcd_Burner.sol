275cd161f01d34b9688fc97e9179692bc0e45646
minion <minion>
2023-03-10 05:02:46 +0000

    with 💕

---
+++ contracts/mainnet/cc/cc2c5c84d397584c9b6ed3135a99e779b6c9efcd_Burner.sol
@@ -0,0 +1,15 @@
+/**
+ *Submitted for verification at Optimistic.Etherscan.io on 2023-03-09
+*/
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.12;
+
+contract Burner {
+    string public name;
+
+    constructor() public {
+        name = "BrbSwap Buner Contract";
+    }
+}
