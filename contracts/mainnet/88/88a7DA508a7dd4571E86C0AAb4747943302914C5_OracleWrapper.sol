a6f8d6e90002c18bf531626d83f83553834e3a81
minion <minion>
2023-06-04 05:02:29 +0000

    with 💕

---
+++ contracts/mainnet/88/88a7DA508a7dd4571E86C0AAb4747943302914C5_OracleWrapper.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.6;
+
+interface IAggregator {
+  function latestAnswer() external view returns (int256);
+  function latestTimestamp() external view returns (uint256);
+  function latestRound() external view returns (uint256);
+  function getAnswer(uint256 roundId) external view returns (int256);
+  function getTimestamp(uint256 roundId) external view returns (uint256);
+  function decimals() external view returns (uint8);
+  function description() external view returns (string memory);
+
+  event AnswerUpdated(int256 indexed current, uint256 indexed roundId, uint256 updatedAt);
+  event NewRound(uint256 indexed roundId, address indexed startedBy, uint256 startedAt);
+}
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.6;
+
+import "./interfaces/IAggregator.sol";
+
+contract OracleWrapper {
+	IAggregator public immutable ethOracle;
+	IAggregator public immutable underlyingOracle;
+
+	constructor(address _ethOracle, address _underlyingOracle) public {
+		ethOracle = IAggregator(_ethOracle);
+		underlyingOracle = IAggregator(_underlyingOracle);
+	}
+
+	function latestAnswer() external view returns (int256) {
+		int256 ethPricInUSD = ethOracle.latestAnswer();
+		int256 underlyingPriceInETH = underlyingOracle.latestAnswer();
+		return underlyingPriceInETH * ethPricInUSD / 10 ** 18;
+	}
+
+	function latestTimestamp() external view returns (uint256) {
+		return underlyingOracle.latestTimestamp();
+	}
+
+	function decimals() external view returns (uint8) {
+		return underlyingOracle.decimals();
+	}
+
+	function description() external view returns (string memory) {
+		return underlyingOracle.description();
+	}
+}
