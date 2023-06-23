0ed7031b87d907e650a381dc5fdb505e71248441
minion <minion>
2023-06-23 05:04:34 +0000

    with 💕

---
+++ contracts/mainnet/68/680f5da83cc5327203ef6ddec4b941363fa37086_MirrorRendererV2.sol
@@ -0,0 +1,382 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.0;
+
+import "openzeppelin-contracts/contracts/utils/Base64.sol";
+import "openzeppelin-contracts/contracts/utils/Strings.sol";
+
+interface IWritingEditions {
+    function name() external view returns (string memory);
+
+    function symbol() external view returns (string memory);
+
+    function ownerOf(uint256 tokenId) external view returns (address);
+
+    function description() external view returns (string memory);
+
+    function limit() external view returns (uint256);
+
+    function price() external view returns (uint256);
+
+    function contentURI() external view returns (string memory);
+
+    function imageURI() external view returns (string memory);
+
+    function royaltyInfo(
+        uint256 _tokenId,
+        uint256 _salePrice
+    ) external view returns (address receiver, uint256 royaltyAmount);
+
+    function baseDescriptionURI() external view returns (string memory);
+}
+
+interface IOwnable {
+    function owner() external view returns (address);
+}
+
+contract MirrorRendererV2 {
+    /// @notice Get `tokenId` URI or data
+    /// @dev If a renderer is set, call renderer's tokenURI
+    /// @param tokenId The tokenId used to request data
+    function tokenURI(uint256 tokenId) external view returns (string memory) {
+        require(
+            IWritingEditions(msg.sender).ownerOf(tokenId) != address(0),
+            "ERC721: query for nonexistent token"
+        );
+
+        uint256 limit = IWritingEditions(msg.sender).limit();
+
+        // slither-disable-next-line uninitialized-local
+        bytes memory editionNumber;
+        if (limit != 0) {
+            editionNumber = abi.encodePacked("/", Strings.toString(limit));
+        }
+
+        return
+            string(
+                abi.encodePacked(
+                    "data:application/json;base64,",
+                    _getTokenURIJson(tokenId, editionNumber)
+                )
+            );
+    }
+
+    function contractURI() public view returns (string memory) {
+        // Pass in 10_000 as mock "price" to get royalty BPS instead
+        // of royalty amount.
+        (address receiver, uint256 royaltyBPS) = IWritingEditions(msg.sender)
+            .royaltyInfo(1, 10_000);
+
+        return
+            string(
+                abi.encodePacked(
+                    "data:application/json;base64,",
+                    _getContractURIJson(royaltyBPS, receiver)
+                )
+            );
+    }
+
+    // https://ethereum.stackexchange.com/questions/8346/convert-address-to-string/8447#8447
+    function _addressToString(address x) internal pure returns (string memory) {
+        bytes memory s = new bytes(40);
+        for (uint256 i = 0; i < 20; i++) {
+            bytes1 b = bytes1(
+                uint8(uint256(uint160(x)) / (2 ** (8 * (19 - i))))
+            );
+            bytes1 hi = bytes1(uint8(b) / 16);
+            bytes1 lo = bytes1(uint8(b) - 16 * uint8(hi));
+            s[2 * i] = _char(hi);
+            s[2 * i + 1] = _char(lo);
+        }
+        return string(abi.encodePacked("0x", s));
+    }
+
+    function _char(bytes1 b) internal pure returns (bytes1 c) {
+        if (uint8(b) < 10) return bytes1(uint8(b) + 0x30);
+        else return bytes1(uint8(b) + 0x57);
+    }
+
+    /// @notice Forked from Uniswap V3's NFTDescriptor deployed at
+    /// address 0x42B24A95702b9986e82d421cC3568932790A48Ec on Ethereum mainnet.
+    function _escapeQuotes(
+        string memory str
+    ) internal pure returns (string memory) {
+        bytes memory strBytes = bytes(str);
+        uint8 quotesCount = 0;
+        for (uint8 i = 0; i < strBytes.length; i++) {
+            if (strBytes[i] == '"') {
+                quotesCount++;
+            }
+        }
+        if (quotesCount > 0) {
+            bytes memory escapedBytes = new bytes(
+                strBytes.length + (quotesCount)
+            );
+            uint256 index;
+            for (uint8 i = 0; i < strBytes.length; i++) {
+                if (strBytes[i] == '"') {
+                    escapedBytes[index++] = "\\";
+                }
+                escapedBytes[index++] = strBytes[i];
+            }
+            return string(escapedBytes);
+        }
+        return str;
+    }
+
+    function _getTokenURIJson(
+        uint256 tokenId,
+        bytes memory editionNumber
+    ) internal view returns (string memory) {
+        return
+            Base64.encode(
+                bytes(
+                    string(
+                        abi.encodePacked(
+                            '{"name": "',
+                            _escapeQuotes(IWritingEditions(msg.sender).name()),
+                            " ",
+                            Strings.toString(tokenId),
+                            editionNumber,
+                            '", "description": "',
+                            _escapeQuotes(
+                                IWritingEditions(msg.sender).description()
+                            ),
+                            '", "content": "ar://',
+                            IWritingEditions(msg.sender).contentURI(),
+                            '", "animation_url": "',
+                            abi.encodePacked(
+                                "https://mirror.xyz/",
+                                Strings.toString(block.chainid),
+                                "/",
+                                _addressToString(msg.sender),
+                                "/render"
+                            ),
+                            '", "image": "ipfs://',
+                            IWritingEditions(msg.sender).imageURI(),
+                            '", "attributes":[{ "trait_type": "Serial", "value": ',
+                            Strings.toString(tokenId),
+                            "}] }"
+                        )
+                    )
+                )
+            );
+    }
+
+    function _getContractURIJson(
+        uint256 royaltyBPS,
+        address receiver
+    ) internal view returns (string memory) {
+        return
+            Base64.encode(
+                bytes(
+                    string(
+                        abi.encodePacked(
+                            '{"name": "',
+                            _escapeQuotes(IWritingEditions(msg.sender).name()),
+                            '", "description": "',
+                            _escapeQuotes(
+                                IWritingEditions(msg.sender).description()
+                            ),
+                            _getMedia(),
+                            '", "seller_fee_basis_points": ',
+                            Strings.toString(royaltyBPS),
+                            ', "fee_recipient": "',
+                            _addressToString(receiver),
+                            '", "external_link": "',
+                            IWritingEditions(msg.sender).baseDescriptionURI(),
+                            '"}'
+                        )
+                    )
+                )
+            );
+    }
+
+    function _getMedia() internal view returns (string memory) {
+        return
+            string(
+                abi.encodePacked(
+                    '", "content": "ar://',
+                    IWritingEditions(msg.sender).contentURI(),
+                    '", "animation_url": "',
+                    abi.encodePacked(
+                        "https://mirror.xyz/",
+                        Strings.toString(block.chainid),
+                        "/",
+                        _addressToString(msg.sender),
+                        "/render"
+                    ),
+                    '", "image": "ipfs://',
+                    IWritingEditions(msg.sender).imageURI()
+                )
+            );
+    }
+}
+
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts (last updated v4.7.0) (utils/Base64.sol)
+
+pragma solidity ^0.8.0;
+
+/**
+ * @dev Provides a set of functions to operate with Base64 strings.
+ *
+ * _Available since v4.5._
+ */
+library Base64 {
+    /**
+     * @dev Base64 Encoding/Decoding Table
+     */
+    string internal constant _TABLE = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+    /**
+     * @dev Converts a `bytes` to its Bytes64 `string` representation.
+     */
+    function encode(bytes memory data) internal pure returns (string memory) {
+        /**
+         * Inspired by Brecht Devos (Brechtpd) implementation - MIT licence
+         * https://github.com/Brechtpd/base64/blob/e78d9fd951e7b0977ddca77d92dc85183770daf4/base64.sol
+         */
+        if (data.length == 0) return "";
+
+        // Loads the table into memory
+        string memory table = _TABLE;
+
+        // Encoding takes 3 bytes chunks of binary data from `bytes` data parameter
+        // and split into 4 numbers of 6 bits.
+        // The final Base64 length should be `bytes` data length multiplied by 4/3 rounded up
+        // - `data.length + 2`  -> Round up
+        // - `/ 3`              -> Number of 3-bytes chunks
+        // - `4 *`              -> 4 characters for each chunk
+        string memory result = new string(4 * ((data.length + 2) / 3));
+
+        /// @solidity memory-safe-assembly
+        assembly {
+            // Prepare the lookup table (skip the first "length" byte)
+            let tablePtr := add(table, 1)
+
+            // Prepare result pointer, jump over length
+            let resultPtr := add(result, 32)
+
+            // Run over the input, 3 bytes at a time
+            for {
+                let dataPtr := data
+                let endPtr := add(data, mload(data))
+            } lt(dataPtr, endPtr) {
+
+            } {
+                // Advance 3 bytes
+                dataPtr := add(dataPtr, 3)
+                let input := mload(dataPtr)
+
+                // To write each character, shift the 3 bytes (18 bits) chunk
+                // 4 times in blocks of 6 bits for each character (18, 12, 6, 0)
+                // and apply logical AND with 0x3F which is the number of
+                // the previous character in the ASCII table prior to the Base64 Table
+                // The result is then added to the table to get the character to write,
+                // and finally write it in the result pointer but with a left shift
+                // of 256 (1 byte) - 8 (1 ASCII char) = 248 bits
+
+                mstore8(resultPtr, mload(add(tablePtr, and(shr(18, input), 0x3F))))
+                resultPtr := add(resultPtr, 1) // Advance
+
+                mstore8(resultPtr, mload(add(tablePtr, and(shr(12, input), 0x3F))))
+                resultPtr := add(resultPtr, 1) // Advance
+
+                mstore8(resultPtr, mload(add(tablePtr, and(shr(6, input), 0x3F))))
+                resultPtr := add(resultPtr, 1) // Advance
+
+                mstore8(resultPtr, mload(add(tablePtr, and(input, 0x3F))))
+                resultPtr := add(resultPtr, 1) // Advance
+            }
+
+            // When data `bytes` is not exactly 3 bytes long
+            // it is padded with `=` characters at the end
+            switch mod(mload(data), 3)
+            case 1 {
+                mstore8(sub(resultPtr, 1), 0x3d)
+                mstore8(sub(resultPtr, 2), 0x3d)
+            }
+            case 2 {
+                mstore8(sub(resultPtr, 1), 0x3d)
+            }
+        }
+
+        return result;
+    }
+}
+
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts (last updated v4.7.0) (utils/Strings.sol)
+
+pragma solidity ^0.8.0;
+
+/**
+ * @dev String operations.
+ */
+library Strings {
+    bytes16 private constant _HEX_SYMBOLS = "0123456789abcdef";
+    uint8 private constant _ADDRESS_LENGTH = 20;
+
+    /**
+     * @dev Converts a `uint256` to its ASCII `string` decimal representation.
+     */
+    function toString(uint256 value) internal pure returns (string memory) {
+        // Inspired by OraclizeAPI's implementation - MIT licence
+        // https://github.com/oraclize/ethereum-api/blob/b42146b063c7d6ee1358846c198246239e9360e8/oraclizeAPI_0.4.25.sol
+
+        if (value == 0) {
+            return "0";
+        }
+        uint256 temp = value;
+        uint256 digits;
+        while (temp != 0) {
+            digits++;
+            temp /= 10;
+        }
+        bytes memory buffer = new bytes(digits);
+        while (value != 0) {
+            digits -= 1;
+            buffer[digits] = bytes1(uint8(48 + uint256(value % 10)));
+            value /= 10;
+        }
+        return string(buffer);
+    }
+
+    /**
+     * @dev Converts a `uint256` to its ASCII `string` hexadecimal representation.
+     */
+    function toHexString(uint256 value) internal pure returns (string memory) {
+        if (value == 0) {
+            return "0x00";
+        }
+        uint256 temp = value;
+        uint256 length = 0;
+        while (temp != 0) {
+            length++;
+            temp >>= 8;
+        }
+        return toHexString(value, length);
+    }
+
+    /**
+     * @dev Converts a `uint256` to its ASCII `string` hexadecimal representation with fixed length.
+     */
+    function toHexString(uint256 value, uint256 length) internal pure returns (string memory) {
+        bytes memory buffer = new bytes(2 * length + 2);
+        buffer[0] = "0";
+        buffer[1] = "x";
+        for (uint256 i = 2 * length + 1; i > 1; --i) {
+            buffer[i] = _HEX_SYMBOLS[value & 0xf];
+            value >>= 4;
+        }
+        require(value == 0, "Strings: hex length insufficient");
+        return string(buffer);
+    }
+
+    /**
+     * @dev Converts an `address` with fixed length of 20 bytes to its not checksummed ASCII `string` hexadecimal representation.
+     */
+    function toHexString(address addr) internal pure returns (string memory) {
+        return toHexString(uint256(uint160(addr)), _ADDRESS_LENGTH);
+    }
+}
